8d56587dcb441b4d7571ff3ba2a5ca44e0b845d6
tobyk <tobiasz.kowalczyk@outlook.com>
2016-04-18 09:45:09 +0200

    Fixed architecture for DNX in the build script

--- ci_build.ps1
+++ ci_build.ps1
@@ -1,5 +1,5 @@
 dnvm install 1.0.0-rc1-update1
-dnvm use 1.0.0-rc1-update1 -a x64 -r coreclr
+dnvm use 1.0.0-rc1-update1 -a x86 -r coreclr
 dnu restore
  #run the build
 & "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" src\QuizzMan.sln
