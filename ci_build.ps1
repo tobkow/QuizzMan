d4436c6bc022733da5d32d7fbf9a72c4586e79ae
tobyk <tobiasz.kowalczyk@outlook.com>
2016-04-18 10:14:47 +0200

    Changes to build script and project file
    Added copyright and version replacing code in the build script, added copyright information to project.json.

--- ci_build.ps1
+++ ci_build.ps1
@@ -4,5 +4,17 @@ $DNVM = "dnvm"
 & $DNVM install 1.0.0-rc1-update1
 & $DNVM use 1.0.0-rc1-update1
 & $DNU restore
+
+# Set the version number in package.json
+$ProjectJsonPath = Join-Path -Path $SolutionRoot -ChildPath "src\Smidge\project.json"
+(gc -Path $ProjectJsonPath) `
+	-replace "(?<=`"version`":\s`")[.\w-]*(?=`",)", "$ReleaseVersionNumber$PreReleaseName" |
+	sc -Path $ProjectJsonPath -Encoding UTF8
+# Set the copyright
+$DateYear = (Get-Date).year
+(gc -Path $ProjectJsonPath) `
+	-replace "(?<=`"copyright`":\s`")[\w\s©]*(?=`",)", "Copyright © Tobiasz Kowalczyk $DateYear" |
+	sc -Path $ProjectJsonPath -Encoding UTF8
+
  #run the build
 $ $DNU build
