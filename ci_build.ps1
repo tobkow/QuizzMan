09d681339dafc58933fe32077b089287a07cf416
tobyk <tobiasz.kowalczyk@outlook.com>
2016-04-18 09:58:26 +0200

    Fixed a typo

--- ci_build.ps1
+++ ci_build.ps1
@@ -2,7 +2,7 @@ $DNU = "dnu"
 $DNVM = "dnvm"
 
 & $DNVM install 1.0.0-rc1-update1
-& $DNVMm use 1.0.0-rc1-update1
+& $DNVM use 1.0.0-rc1-update1
 & $DNU restore
  #run the build
 & "C:\Program Files (x86)\MSBuild\14.0\Bin\MSBuild.exe" src\QuizzMan.sln
